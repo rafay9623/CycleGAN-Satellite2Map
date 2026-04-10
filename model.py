b07c2bd0fa1337bfb48fad07489c4894fadd7d07
Muhammad Adeel Umar <adeelumar30@hotmail.com>
2026-04-10 18:47:24 +0500

    Implement Generator and ResNetBlock classes

---
+++ model.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+
+class ResNetBlock(nn.Module):
+    def __init__(self, channels):
+        super(ResNetBlock, self).__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(channels, channels, kernel_size=3, padding=1),
+            nn.InstanceNorm2d(channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channels, channels, kernel_size=3, padding=1),
+            nn.InstanceNorm2d(channels)
+        )
+    def forward(self, x):
+        return x + self.block(x)
+
+class Generator(nn.Module):
+    def __init__(self, num_residual_blocks=6):
+        super(Generator, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, padding=3),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
+            nn.InstanceNorm2d(256),
+            nn.ReLU(inplace=True)
+        )
+        self.transformer = nn.Sequential(
+            *[ResNetBlock(256) for _ in range(num_residual_blocks)]
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 3, kernel_size=7, padding=3),
+            nn.Tanh()
+        )
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.transformer(x)
+        x = self.decoder(x)
+        return x
